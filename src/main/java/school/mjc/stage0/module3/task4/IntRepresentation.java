daa77353a414e1dc5b7d65b944c9655b2ee644d8
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-18 08:11:08 +0300

    hashdas

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.module3.task4;
 
+import java.util.Arrays;
+
 public class IntRepresentation {
     public static void main(String[] args) {
+        int a = 99;
+        int b = 104;
+        int c = 97;
+        int d = 114;
+        System.out.print((char) a);
+        System.out.print((char) b);
+        System.out.print((char) c);
+        System.out.print((char) d);
     }
 }
